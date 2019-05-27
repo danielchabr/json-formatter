319fa0514ed9ca5ef6cfbe10bbdb3c6970138511
Daniel Chabr <danielchabr@gmail.com>
2019-05-27 07:39:30 +0200

    normalize with normalizr schema

--- src/redux/data/actions.ts
+++ src/redux/data/actions.ts
@@ -3,6 +3,8 @@ import { Dispatch, Action } from 'redux'
 import { Entity, EntityInput } from './types'
 import { EntityMap } from './types'
 import { processEntities } from './entityUtils'
+import { normalize, schema } from 'normalizr'
+import uuid from 'uuid'
 
 export const removeEntity = createAction<string>('remove entity')
 export const setEntities = createAction<{ [id: string]: Entity }>(
@@ -22,7 +24,8 @@ export const loadFileAsync = (file: File) => {
             const content = fileReader.result as string
             try {
                 const parsedData = JSON.parse(content)
-                normalizeDataAsync(parsedData)(dispatch)
+                // normalizeDataAsync(parsedData)(dispatch)
+                normalizrAsync(parsedData)(dispatch)
             } catch (error) {
                 dispatch(setLoadingError(error && error.message))
             }
@@ -33,6 +36,36 @@ export const loadFileAsync = (file: File) => {
         }
         fileReader.readAsText(file)
     }
+}
+
+export const normalizrAsync = (data: EntityInput[]) => (
+    dispatch: Dispatch<Action<any>>
+) => {
+    const entityMap: EntityMap = {}
+    const IDs: string[] = []
+
+    const entity = new schema.Entity(
+        'entity',
+        {},
+        {
+            idAttribute: 'id',
+            processStrategy: (value) => {
+                if (!Object.prototype.hasOwnProperty.call(value, 'id')) {
+                    value.id = uuid()
+                }
+                return { ...value }
+            },
+        }
+    )
+    const values = new schema.Values({
+        records: new schema.Array(entity),
+    })
+    entity.define({ kids: values })
+    const normalizedData = normalize(data, [entity])
+
+    dispatch(setEntities(normalizedData.entities.entity))
+    dispatch(setRootEntityList(normalizedData.result))
+    dispatch(setLoadingSuccess())
 }
 
 export const normalizeDataAsync = (data: EntityInput[]) => (
